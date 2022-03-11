e0a5b213efa8c0588496d9c92b41458866860a3c
Jasmine Thai <jtt9fb@umsystem.edu>
2022-03-11 01:30:57 -0600

    added graphs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from myApartmentFuncs import *
 from ApartmentClass import *
+import matplotlib.pyplot as plt
+import pandas as pd
 
 present = date.today()
 
@@ -9,6 +11,15 @@ firstMonthAmt, payDate, week = brokeCheck(renter)
 
 yearReceipt = yearSimulator(renter,payDate, week)
 
-print(yearReceipt)
+data = {'date': yearReceipt.keys(),
+        'balance': yearReceipt.values()}
+new = pd.DataFrame.from_dict(data)
 
+df = pd.DataFrame(new, columns=['date', 'balance'])
 
+plt.plot(df['date'], df['balance'], color='red', marker='o')
+plt.title('balance Vs date', fontsize=14)
+plt.xlabel('date', fontsize=14)
+plt.ylabel('balance', fontsize=14)
+plt.grid(True)
+plt.show()
